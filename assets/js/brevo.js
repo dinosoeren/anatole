8cd0610205ebd82d566138f8dee62890ae6e4e49
soeren <soeren@fractal.is>
2025-08-24 14:52:31 -0400

    fix: contact form recaptcha

--- assets/js/brevo.js
+++ assets/js/brevo.js
@@ -1,48 +1,59 @@
-window.REQUIRED_CODE_ERROR_MESSAGE = 'Please choose a country code';
-window.LOCALE = 'en';
+window.REQUIRED_CODE_ERROR_MESSAGE = "Please choose a country code";
+window.LOCALE = "en";
 window.EMAIL_INVALID_MESSAGE = window.SMS_INVALID_MESSAGE =
-  'The information provided is invalid. Please review the field format and try again.';
+  "The information provided is invalid. Please review the field format and try again.";
 
-window.REQUIRED_ERROR_MESSAGE = 'This field cannot be left blank. ';
+window.REQUIRED_ERROR_MESSAGE = "This field cannot be left blank. ";
 
-window.GENERIC_INVALID_MESSAGE = 'The information provided is invalid. Please review the field format and try again.';
+window.GENERIC_INVALID_MESSAGE =
+  "The information provided is invalid. Please review the field format and try again.";
 
 window.translation = {
   common: {
-    selectedList: '{quantity} list selected',
-    selectedLists: '{quantity} lists selected',
-    selectedOption: '{quantity} selected',
-    selectedOptions: '{quantity} selected',
+    selectedList: "{quantity} list selected",
+    selectedLists: "{quantity} lists selected",
+    selectedOption: "{quantity} selected",
+    selectedOptions: "{quantity} selected",
   },
 };
 
 var AUTOHIDE = Boolean(1);
 
 // Listen for email input events to show disclosure and load recaptcha
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   let loadedReCaptcha = false;
-  const formContainer = document.getElementById('sib-form-container');
-  if (!formContainer) {
+  const formContainer = document.getElementById("sib-form-container");
+  const contactForm = document.getElementById("contact-form");
+  if (!formContainer && !contactForm) {
     return;
   }
 
-  const emailInput = document.getElementById('EMAIL');
-  const optinContainer = document.querySelector('.sib-optin');
-  const declarationContainer = document.querySelector('.sib-form__declaration');
-  const subscribeButton = document.querySelector('.sib-form-block__button');
-  const optinCheckbox = document.getElementById('OPT_IN');
+  const emailInput = document.getElementById("EMAIL");
+  const contactEmailInput = document.getElementById("email");
+  const optinContainer = document.querySelector(".sib-optin");
+  const declarationContainer = document.querySelector(".sib-form__declaration");
+  const subscribeButton = document.querySelector(".sib-form-block__button");
+  const optinCheckbox = document.getElementById("OPT_IN");
 
-  if (!emailInput || !optinContainer || !declarationContainer || !subscribeButton || !optinCheckbox) {
+  if (
+    !contactEmailInput &&
+    (!emailInput ||
+      !optinContainer ||
+      !declarationContainer ||
+      !subscribeButton ||
+      !optinCheckbox)
+  ) {
     return;
   }
 
   function loadReCaptcha() {
     if (loadedReCaptcha) return;
-    if (formContainer) {
-      const scriptEl = document.createElement('script');
-      scriptEl.src = 'https://www.google.com/recaptcha/api.js?hl=en&trustedtypes=true';
+    if (formContainer || contactForm) {
+      const scriptEl = document.createElement("script");
+      scriptEl.src =
+        "https://www.google.com/recaptcha/api.js?hl=en&trustedtypes=true";
 
-      let parent = formContainer.parentNode;
+      let parent = (formContainer ?? contactForm).parentNode;
       if (parent.parentNode) parent = parent.parentNode;
       parent.appendChild(scriptEl);
 
@@ -50,8 +61,10 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   }
 
-  subscribeButton.disabled = true;
-  subscribeButton.classList.add('is-disabled');
+  if (subscribeButton) {
+    subscribeButton.disabled = true;
+    subscribeButton.classList.add("is-disabled");
+  }
 
   const validateEmail = (email) => {
     const re =
@@ -59,28 +72,42 @@ document.addEventListener('DOMContentLoaded', function () {
     return re.test(String(email).toLowerCase());
   };
 
-  const checkFormValidity = () => {
-    const isEmailValid = validateEmail(emailInput.value);
-    const isOptinChecked = optinCheckbox.checked;
+  const checkFormValidity = (input) => {
+    const isEmailValid = validateEmail(input.value);
 
-    if (isEmailValid) {
-      optinContainer.classList.add('is-visible');
-      declarationContainer.classList.add('is-visible');
+    if (optinCheckbox) {
+      const isOptinChecked = optinCheckbox.checked;
+
+      if (isEmailValid) {
+        optinContainer.classList.add("is-visible");
+        declarationContainer.classList.add("is-visible");
+        loadReCaptcha();
+      } else {
+        optinContainer.classList.remove("is-visible");
+        declarationContainer.classList.remove("is-visible");
+      }
+
+      if (isEmailValid && isOptinChecked) {
+        subscribeButton.disabled = false;
+        subscribeButton.classList.remove("is-disabled");
+      } else {
+        subscribeButton.disabled = true;
+        subscribeButton.classList.add("is-disabled");
+      }
+    } else if (isEmailValid) {
       loadReCaptcha();
-    } else {
-      optinContainer.classList.remove('is-visible');
-      declarationContainer.classList.remove('is-visible');
-    }
-
-    if (isEmailValid && isOptinChecked) {
-      subscribeButton.disabled = false;
-      subscribeButton.classList.remove('is-disabled');
-    } else {
-      subscribeButton.disabled = true;
-      subscribeButton.classList.add('is-disabled');
     }
   };
 
-  emailInput.addEventListener('input', checkFormValidity);
-  optinCheckbox.addEventListener('change', checkFormValidity);
+  if (contactEmailInput) {
+    contactEmailInput.addEventListener("input", () =>
+      checkFormValidity(contactEmailInput)
+    );
+  }
+  if (emailInput) {
+    emailInput.addEventListener("input", () => checkFormValidity(emailInput));
+    optinCheckbox.addEventListener("change", () =>
+      checkFormValidity(emailInput)
+    );
+  }
 });
